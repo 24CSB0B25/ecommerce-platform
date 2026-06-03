6b1d8a73ab86765b9b66d7c6cbb3b9b55cabd74c
24CSB0B25 <hm24csb0b25@student.nitw.ac.in>
2026-06-03 15:30:23 +0530

    Use Gmail SMTP port 587

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -8,14 +8,14 @@ const sendEmail = async (
   try {
     const transporter =
       nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
-          user:
-            process.env.EMAIL_USER,
-          pass:
-            process.env.EMAIL_PASS,
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
         },
-      });
+    });
 
     console.log(
       "Sending email to:",
